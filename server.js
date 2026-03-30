c3d42bdd52a5da6bf7fc9d88ec1476eef3474c66
Nitesh Kumar <hash.bitcoder@gmail.com>
2026-03-30 08:42:40 +0530

    Fix server.js to use correct IP address format

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.listen(process.env.PORT,0.0.0.0, () => {
+app.listen(process.env.PORT,"0.0.0.0", () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
